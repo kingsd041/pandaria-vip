a2ad0be710f37972b658401a48281794b8531f79
jianghang8421 <jianghang8421@163.com>
2019-06-17 11:21:37 +0800

    PANDARIA: Append username for auditlog query datas

--- pkg/httpproxy/proxy_auditlog.go
+++ pkg/httpproxy/proxy_auditlog.go
@@ -1,7 +1,11 @@
 package httpproxy
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httputil"
 
@@ -87,6 +91,67 @@ func NewAuditlogProxy(prefix string, validHosts Supplier, scaledContext *config.
 			if err := p.proxy(req); err != nil {
 				logrus.Errorf("[AuditlogProxy] Failed to proxy %v: %v", req, err)
 			}
+		},
+		ModifyResponse: func(resp *http.Response) error {
+			var logCollection map[string]interface{}
+			err := json.NewDecoder(resp.Body).Decode(&logCollection)
+			if err != nil {
+				logrus.Errorf("[AuditlogProxy] Decode response body error: %v", err)
+				return err
+			}
+
+			var resourceType string
+			res, getTypeOK := logCollection["resourceType"]
+			if res != nil && getTypeOK {
+				result, ok := res.(string)
+				if ok {
+					resourceType = result
+				}
+			}
+
+			if resourceType == "Auditlog" {
+				data, getDataOK := logCollection["data"]
+				if !getDataOK {
+					logrus.Errorln("[AuditlogProxy] Get logCollection data error")
+					return errors.New("Get logCollection data error")
+				}
+
+				dataList, dataToListOK := data.([]interface{})
+				if !dataToListOK {
+					logrus.Errorln("[AuditlogProxy] Get logCollection data error")
+					return errors.New("Get logCollection data error")
+				}
+				if len(dataList) > 0 {
+					userList, _ := scaledContext.Management.Users("").List(metav1.ListOptions{})
+					for _, v := range dataList {
+						var username string
+						userID := v.(map[string]interface{})["userID"]
+						if userID != "" {
+							for _, user := range userList.Items {
+								if user.Name == userID {
+									username = user.Username
+								}
+							}
+						}
+						if username != "" {
+							v.(map[string]interface{})["userName"] = username
+						} else {
+							v.(map[string]interface{})["userName"] = userID
+						}
+					}
+				}
+			}
+
+			result, marshalErr := json.Marshal(logCollection)
+			if marshalErr != nil {
+				logrus.Errorf("[AuditlogProxy] Marshal data error: %v", marshalErr)
+				return marshalErr
+			}
+			resp.Body.Close()
+			buf := bytes.NewBuffer(result)
+			resp.Body = ioutil.NopCloser(buf)
+			resp.Header["Content-Length"] = []string{fmt.Sprint(buf.Len())}
+			return nil
 		},
 	}
 }
