8f265d2ce6e653bf9f5ed1bc984d21b09ca94e24
wardenlym <wardenlym@gmail.com>
2019-06-27 16:45:32 +0800

    PANDARIA: add multus-canal addon for supporting network policy

--- pkg/controllers/management/clusterprovisioner/networkaddons.go
+++ pkg/controllers/management/clusterprovisioner/networkaddons.go
@@ -10,7 +10,10 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-const pluginMultusFlannel = "multus-flannel-macvlan"
+const (
+	pluginMultusFlannel = "multus-flannel-macvlan"
+	pluginMultusCanal   = "multus-canal-macvlan"
+)
 
 func (p *Provisioner) handleNetworkPlugin(old v3.ClusterSpec) (v3.ClusterSpec, error) {
 	spec := old.DeepCopy()
@@ -19,6 +22,9 @@ func (p *Provisioner) handleNetworkPlugin(old v3.ClusterSpec) (v3.ClusterSpec, e
 		switch spec.RancherKubernetesEngineConfig.Network.Plugin {
 		case pluginMultusFlannel:
 			err := p.handleMultusFlannel(spec.RancherKubernetesEngineConfig)
+			return *spec, err
+		case pluginMultusCanal:
+			err := p.handleMultusCanal(spec.RancherKubernetesEngineConfig)
 			return *spec, err
 		}
 	}
@@ -61,6 +67,46 @@ func (p *Provisioner) handleMultusFlannel(cfg *v3.RancherKubernetesEngineConfig)
 func applyMultusFlannelOption(addons string, option map[string]string) string {
 	if option["flannel_iface"] != "" {
 		addons = strings.Replace(addons, "# - --iface=eth0", fmt.Sprintf("- --iface=%s", option["flannel_iface"]), -1)
+	}
+
+	return addons
+}
+
+func (p *Provisioner) handleMultusCanal(cfg *v3.RancherKubernetesEngineConfig) error {
+	template := fmt.Sprintf("%s%s%s.yaml",
+		os.Getenv("NETWORK_ADDONS_DIR"), string(os.PathSeparator), pluginMultusCanal)
+
+	if _, err := os.Stat(template); err != nil {
+		logrus.Error(err)
+		return err
+	}
+
+	b, err := ioutil.ReadFile(template)
+	if err != nil {
+		logrus.Error(err)
+		return err
+	}
+
+	content := applyMultusCanalOption(string(b), cfg.Network.Options)
+	path := fmt.Sprintf("%s.current", template)
+	err = ioutil.WriteFile(path, []byte(content), 0644)
+	if err != nil {
+		logrus.Error(err)
+		return err
+	}
+
+	// rewrite network option and insert addons_include
+	cfg.Network.Plugin = "none"
+	if cfg.AddonsInclude == nil {
+		cfg.AddonsInclude = []string{}
+	}
+	cfg.AddonsInclude = append([]string{path}, cfg.AddonsInclude...)
+	return nil
+}
+
+func applyMultusCanalOption(addons string, option map[string]string) string {
+	if option["canal_iface"] != "" {
+		addons = strings.Replace(addons, "canal_iface: \"\"", fmt.Sprintf("canal_iface: \"%s\"", option["canal_iface"]), -1)
 	}
 
 	return addons
