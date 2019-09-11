e37e38deeeca7bfd65f28ddf5f8cc62cb0fe3e08
niusmallnan <niusmallnan@gmail.com>
2019-09-11 10:17:07 +0800

    PANDARIA: add PANDARIA_DEV_MODE env for UI developers

---
+++ pkg/httpproxy/panda_utils.go
@@ -0,0 +1,20 @@
+package httpproxy
+
+import (
+	"net/url"
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+func unescapePath(destPath string) (string, error) {
+	// add for pandaria UI dev
+	if os.Getenv("PANDARIA_DEV_MODE") != "" {
+		destPath, err := url.QueryUnescape(destPath)
+		logrus.Infof("******* proxy url: %v ********", destPath)
+		if err != nil {
+			return "", err
+		}
+	}
+	return destPath, nil
+}
