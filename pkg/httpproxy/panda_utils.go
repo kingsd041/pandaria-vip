dc3b52dc91eb37bbd772344685e4ee2ae84338af
JacieChao <kathyyy@163.com>
2019-09-23 16:06:28 +0800

    PANDARIA: Fix destPath not replaced with decode value after setting PANDARIA_DEV_MODE env

--- pkg/httpproxy/panda_utils.go
+++ pkg/httpproxy/panda_utils.go
@@ -9,8 +9,9 @@ import (
 
 func unescapePath(destPath string) (string, error) {
 	// add for pandaria UI dev
+	var err error
 	if os.Getenv("PANDARIA_DEV_MODE") != "" {
-		destPath, err := url.QueryUnescape(destPath)
+		destPath, err = url.QueryUnescape(destPath)
 		logrus.Infof("******* proxy url: %v ********", destPath)
 		if err != nil {
 			return "", err
