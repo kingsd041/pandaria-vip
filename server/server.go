cea4197116071efff39df9ed83a459d5552f9e7c
niusmallnan <niusmallnan@gmail.com>
2019-10-16 15:31:31 +0800

    PANDARIA: Vsphere Lookup API
    Backport PR: https://github.com/rancher/rancher/pull/23364

--- server/server.go
+++ server/server.go
@@ -8,6 +8,7 @@ import (
 	"github.com/rancher/rancher/pkg/api/controllers/dynamiclistener"
 	"github.com/rancher/rancher/pkg/api/customization/clusterregistrationtokens"
 	"github.com/rancher/rancher/pkg/api/customization/gpusharing"
+	"github.com/rancher/rancher/pkg/api/customization/vsphere"
 	managementapi "github.com/rancher/rancher/pkg/api/server"
 	"github.com/rancher/rancher/pkg/audit"
 	"github.com/rancher/rancher/pkg/auth/providers/publicapi"
@@ -125,6 +126,7 @@ func newAuthed(tokenAPI http.Handler, managementAPI http.Handler, k8sproxy http.
 	authed.Path("/meta/gkeServiceAccounts").Handler(capabilities.NewGKEServiceAccountsHandler())
 	authed.Path("/meta/aksVersions").Handler(capabilities.NewAKSVersionsHandler())
 	authed.Path("/meta/aksVirtualNetworks").Handler(capabilities.NewAKSVirtualNetworksHandler())
+	authed.Path("/meta/vsphere/{field}").Handler(vsphere.NewVsphereHandler(scaledContext))
 	authed.PathPrefix("/meta/proxy").Handler(newProxy(scaledContext))
 	authed.PathPrefix("/meta/harbor").Handler(newHarborProxy(scaledContext))
 	authed.PathPrefix("/meta/auditlog").Handler(newAuditlogProxy(scaledContext))
